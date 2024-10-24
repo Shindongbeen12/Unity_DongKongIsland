d35fd3b2e9abd0984e9d556755ebb87ecc66bf25
shindongbeen <127067087+Shindongbeen12@users.noreply.github.com>
2024-10-25 04:30:17 +0900

    타이틀 추가

---
+++ Assets/코드/버튼코드/ButtonBasic.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBasic : MonoBehaviour
+{
+    public void Story()
+    {
+        Debug.Log("스토리 버튼");
+    }
+    public void Free()
+    {
+        Debug.Log("자유 버튼");
+    }
+}
