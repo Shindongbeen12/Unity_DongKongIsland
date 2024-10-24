00776e84253916d92e8bafc30021352f0f98c037
shindongbeen <127067087+Shindongbeen12@users.noreply.github.com>
2024-10-25 00:57:53 +0900

    대화 다이로그 변경

--- Assets/코드/버튼코드/ButtonColorController.cs
+++ Assets/코드/버튼코드/ButtonColorController.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 public class ButtonColorController : MonoBehaviour
 {
     private Button button;
-    [SerializeField] public GameObject Talk;
+    [SerializeField] public GameObject Talk1;
+    [SerializeField] public GameObject Talk2;
     public bool TalkActive = true;
     void Start()
     {
@@ -16,12 +17,14 @@ public class ButtonColorController : MonoBehaviour
         
         if(TalkActive == true)
         {
-            Talk.SetActive(false);
+            Talk1.SetActive(false);
+            Talk2.SetActive(false);
             TalkActive = false;
         }
         else if(TalkActive == false)
         {
-            Talk.SetActive(true);
+            Talk1.SetActive(true);
+            Talk2.SetActive(true);
             TalkActive = true;
         }
         
