0afdfdd6f2ac312a3611671c6cbf19dbb5c389f1
shindongbeen <127067087+Shindongbeen12@users.noreply.github.com>
2024-10-31 14:21:24 +0900

    123

--- Assets/코드/캐릭터 이동 코드/ButtonMove.cs
+++ Assets/코드/캐릭터 이동 코드/ButtonMove.cs
@@ -8,81 +8,126 @@ public class ButtonMove : MonoBehaviour
     Rigidbody2D rid2D;
     public GameObject Character;
     public bool LeftUp;
-    public bool Up;
+    public bool Up = false;
     public bool RightUp;
-    public bool Left;
-    public bool Right;
+    public bool Left = false;
+    public bool Right = false;
     public bool LeftDown;
-    public bool Down;
+    public bool Down = false;
     public bool RightDown;
-
-    public float y1 = 11.0f;
-
-    
-
-
     void Start()
     {
-        rid2D = GetComponent<Rigidbody2D>();
+        rid2D = GetComponent<Rigidbody2D>();    
+        
     }
 
-    // 버튼 눌림/떼기 함수들...
-    public void LeftUpTrueButton() { LeftUp = true; }
-    public void LeftUpFalseButton() { LeftUp = false; }
-    public void UpTrueButton() { Up = true; }
-    public void UpfalseButton() { Up = false; }
-    public void RightUpTrueButton() { RightUp = true; }
-    public void RightUpFalseButton() { RightUp = false; }
-    public void LeftTrueButton() { Left = true; }
-    public void LeftfalseButton() { Left = false; }
-    public void RightTrueButton() { Right = true; }
-    public void RightFalseButton() { Right = false; }
-    public void LeftDownTrueButton() { LeftDown = true; }
-    public void LeftDownFalseButton() { LeftDown = false; }
-    public void DownTrueButton() { Down = true; }
-    public void DownFalseButton() { Down = false; }
-    public void RightDownTrueButton() { RightDown = true; }
-    public void RightDownFalseButton() { RightDown = false; }
+
+    public void LeftUpTrueButton()
+    {
+        LeftUp = true;
+    }
+    public void LeftUpFalseButton()
+    {
+        LeftUp = false;
+    }
+    public void UpTrueButton()
+    {
+        Up = true;
+    }
+    public void UpfalseButton()
+    {
+        Up = false;
+    }
+    public void RightUpTrueButton()
+    {
+        RightUp = true;
+    }
+    public void RightUpFalseButton()
+    {
+        RightUp = false;
+    }
+    public void LeftTrueButton()
+    {
+        Left = true;
+    }
+    public void LeftfalseButton()
+    {
+        Left = false;
+    }
+
+    public void RightTrueButton()
+    {
+        Right = true;
+    }
+    public void RightFalseButton()
+    {
+        Right = false;
+    }
+    public void LeftDownTrueButton()
+    {
+        LeftDown = true;
+    }
+    public void LeftDownFalseButton()
+    {
+        LeftDown = false;
+    }
+    public void DownTrueButton()
+    {
+        Down = true;
+    }
+    public void DownFalseButton()
+    {
+        Down = false;
+    }
+    public void RightDownTrueButton()
+    {
+        RightDown = true;
+    }
+    public void RightDownFalseButton()
+    {
+        RightDown = false;
+    }
 
     void Update()
     {
-        Vector3 newPosition = Character.transform.position;
-
-        // 각 방향에 대해 제한된 범위 내에서만 이동
-        if (LeftUp && newPosition.x > -11.0f && newPosition.y < y1)
+        if (LeftUp == true)
         {
-            newPosition += new Vector3(-speed * Time.deltaTime, speed * Time.deltaTime, 0);
+            Character.transform.Translate(new Vector3(-speed * Time.deltaTime, speed * Time.deltaTime, 0));
         }
-        if (Up && newPosition.y < y1)
+        if (Up == true)
         {
-            newPosition += new Vector3(0, speed * Time.deltaTime, 0);
+            Character.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         }
-        if (RightUp && newPosition.x < 11.0f && newPosition.y < y1)
+        if (RightUp == true)
         {
-            newPosition += new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+            Character.transform.Translate(new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0));
         }
-        if (Left && newPosition.x > -11.0f)
+        if (Left == true)
         {
-            newPosition += new Vector3(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Right && newPosition.x < 11.0f)
-        {
-            newPosition += new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (LeftDown && newPosition.x > -11.0f)
-        {
-            newPosition += new Vector3(-speed * Time.deltaTime, -speed * Time.deltaTime, 0);
-        }
-        if (Down && newPosition.y > -0.0f)
-        {
-            newPosition += new Vector3(0, -speed * Time.deltaTime, 0);
-        }
-        if (RightDown && newPosition.x < 11.0f)
-        {
-            newPosition += new Vector3(speed * Time.deltaTime, -speed * Time.deltaTime, 0);
+            Character.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
 
-        // 새로운 위치 적용
-        Character.transform.position = newPosition;
+        if (Right == true)
+        {
+            Character.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (LeftDown == true)
+        {
+            Character.transform.Translate(new Vector3(-speed * Time.deltaTime, -speed * Time.deltaTime, 0));
+        }
+
+        if (Down == true)
+        {
+            Character.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (RightDown == true)
+        {
+            Character.transform.Translate(new Vector3(speed * Time.deltaTime, -speed * Time.deltaTime, 0));
+        }
+
+
+
+
+
     }
 }
