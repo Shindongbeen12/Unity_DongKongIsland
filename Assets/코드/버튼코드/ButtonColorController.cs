29ba5f96f61d2e6679950fe4ee93c37e21f5d525
우주대마왕 <127067087+Shindongbeen12@users.noreply.github.com>
2024-10-31 15:42:57 +0900

    Revert "백업"
    This reverts commit ccaa5227a6c2bfb3654af34d36a2bd5b1fdcfff7.

--- Assets/코드/버튼코드/ButtonColorController.cs
+++ Assets/코드/버튼코드/ButtonColorController.cs
@@ -4,7 +4,13 @@ using UnityEngine.UI;
 public class ButtonColorController : MonoBehaviour
 {
     private Button button;
+<<<<<<< HEAD
+    [SerializeField] public GameObject Talk1;
+    [SerializeField] public GameObject Talk2;
+    public bool TalkActive = true;
+=======
 
+>>>>>>> parent of 51286c5 (대화 기능 추가)
     void Start()
     {
         
@@ -12,6 +18,23 @@ public class ButtonColorController : MonoBehaviour
 
     public void OnButtonClick()
     {
+<<<<<<< HEAD
+        
+        if(TalkActive == true)
+        {
+            Talk1.SetActive(false);
+            Talk2.SetActive(false);
+            TalkActive = false;
+        }
+        else if(TalkActive == false)
+        {
+            Talk1.SetActive(true);
+            Talk2.SetActive(true);
+            TalkActive = true;
+        }
+        
+=======
         Debug.Log("��ưŬ��");
+>>>>>>> parent of 51286c5 (대화 기능 추가)
     }
 }
